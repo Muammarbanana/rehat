52f07ab05f09ab0d3cd8cc64e1e1150bf857f362
Muammarbanana <muammarfanani2@gmail.com>
2020-05-02 12:33:07 +0700

    add speech recognizer at login page

--- app/src/main/java/com/example/rehat/Login.kt
+++ app/src/main/java/com/example/rehat/Login.kt
@@ -1,10 +1,17 @@
 package com.example.rehat
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.provider.Settings.Global.putString
+import android.speech.RecognizerIntent
 import android.support.v7.app.AppCompatActivity
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
@@ -14,6 +21,8 @@ class Login : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var ref: DatabaseReference
+    private val SPEECH_REQUEST_CODE = 0
+    private var inputIdentificator = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,14 +41,27 @@ class Login : AppCompatActivity() {
                 Toast.makeText(this, "Tolong isi username dan password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            getUsernameList(object : FirebaseCallBack {
+            passUsername(object : MyCallBack {
                 override fun onCallBack(email: String) {
                     loginUser(email, password)
                 }
             }, namaPengguna)
         }
+
+        val et: EditText = findViewById(R.id.editTextNamaPengguna)
+
+        imgMicrophone1.setOnClickListener {
+            inputIdentificator = 0
+            getVoice()
+        }
+
+        imgMicrophone2.setOnClickListener {
+            inputIdentificator = 1
+            getVoice()
+        }
     }
 
+    // Check if user is logged in or not
     override fun onStart() {
         super.onStart()
         val currentUser = auth.currentUser
@@ -49,6 +71,7 @@ class Login : AppCompatActivity() {
         }
     }
 
+    // Login with email and password
     private fun loginUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -66,7 +89,8 @@ class Login : AppCompatActivity() {
             }
     }
 
-    private fun getUsernameList(firebasecallback: FirebaseCallBack, username: String) {
+    // Read username from database and get the user email for login
+    private fun passUsername(firebasecallback: MyCallBack, username: String) {
         val daftar = mutableListOf<User>()
         ref = FirebaseDatabase.getInstance().getReference("Users")
         ref.addListenerForSingleValueEvent(object: ValueEventListener {
@@ -92,7 +116,31 @@ class Login : AppCompatActivity() {
         })
     }
 
-    private interface FirebaseCallBack {
-        fun onCallBack(email: String)
+    // Get email outside of onDataChange method
+    private interface MyCallBack {
+        fun onCallBack(text: String)
+    }
+
+    private fun getVoice() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        }
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val spokenText: String? =
+                data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).let { results ->
+                    results?.get(0)
+                }
+            // Do something with spokenText
+            when(inputIdentificator) {
+                0 -> editTextNamaPengguna.setText(spokenText)
+                else -> editTextKataSandi.setText(spokenText)
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
