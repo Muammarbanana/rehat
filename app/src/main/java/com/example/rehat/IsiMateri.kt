8c8c51d4e1d2c99725bf4e49f00da460064a8c23
Muammarbanana <muammarfanani2@gmail.com>
2020-05-19 17:15:21 +0700

    materi text layout format as a list

--- app/src/main/java/com/example/rehat/IsiMateri.kt
+++ app/src/main/java/com/example/rehat/IsiMateri.kt
@@ -4,7 +4,11 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.view.View
+import android.widget.Toast
+import androidx.core.text.HtmlCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_isi_materi.*
 
@@ -16,9 +20,17 @@ class IsiMateri : AppCompatActivity() {
 
         judulMateri.text = intent.getStringExtra("Judul")
         Picasso.get().load(intent.getStringExtra("Gambar")).resize(328,191).centerCrop().into(imgMateri)
-        textIsi.text = intent.getStringExtra("Isi")
+        val isi = intent.getStringExtra("Isi")
         imgMateri.contentDescription = intent.getStringExtra("Desk")
         val backgroundcolor = intent.getStringExtra("Warna")
+        val arrayisi = isi.split("\n").toTypedArray()
+
+        rvIsiMateri.setHasFixedSize(true)
+        rvIsiMateri.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        val adapter = Adapter(arrayisi)
+        adapter.notifyDataSetChanged()
+        rvIsiMateri.adapter = adapter
 
         toolbar.background = ColorDrawable(Color.parseColor(backgroundcolor))
         val window = this.window
