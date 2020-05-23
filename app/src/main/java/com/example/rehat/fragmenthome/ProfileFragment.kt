ee8234ea5f0d981fc957afac7ad0ec72657a487a
Muammarbanana <muammarfanani2@gmail.com>
2020-05-23 23:41:44 +0700

    upload profile photo success

--- app/src/main/java/com/example/rehat/fragmenthome/ProfileFragment.kt
+++ app/src/main/java/com/example/rehat/fragmenthome/ProfileFragment.kt
@@ -1,12 +1,18 @@
 package com.example.rehat.fragmenthome
 
+import android.app.Activity.RESULT_OK
+import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.rehat.EditProfil
@@ -14,6 +20,8 @@ import com.example.rehat.R
 import com.example.rehat.WelcomeScreen
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
@@ -27,26 +35,38 @@ class ProfileFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var ref: DatabaseReference
+    private lateinit var storage: StorageReference
+    private lateinit var root: View
+    private lateinit var imageUri: Uri
+    private val PICK_IMAGE_REQUEST = 1;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        root = inflater.inflate(R.layout.fragment_profile, container, false)
 
         auth = FirebaseAuth.getInstance()
-        view.teksKeluar.setOnClickListener {
-            popAlert(view)
-        }
-        getName(view)
-
-        view.teksEditProfile.setOnClickListener {
-            val intent = Intent(view.context, EditProfil::class.java)
-            view.context.startActivity(intent)
+        root.teksKeluar.setOnClickListener {
+            popAlert(root)
         }
 
-        return view
+        storage = FirebaseStorage.getInstance().getReference("profilepic")
+        ref = FirebaseDatabase.getInstance().getReference("Users")
+
+        getName(root)
+
+        root.teksEditProfile.setOnClickListener {
+            val intent = Intent(root.context, EditProfil::class.java)
+            root.context.startActivity(intent)
+        }
+
+        root.imgAddPhoto.setOnClickListener {
+            openImageChooser(root)
+        }
+
+        return root
     }
 
     private fun popAlert(view: View) {
@@ -76,7 +96,6 @@ class ProfileFragment : Fragment() {
     }
 
     private fun getName(view: View) {
-        ref = FirebaseDatabase.getInstance().getReference("Users")
         ref.orderByKey().equalTo(auth.currentUser?.uid!!).addValueEventListener(object: ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
 
@@ -87,12 +106,12 @@ class ProfileFragment : Fragment() {
                     for (h in p0.children) {
                         val name = h.child("nama").value.toString()
                         val photo = h.child("photo").value.toString()
-                        profileName.text = name
-                        if (photo == "null") {
+                        root.profileName.text = name
+                        if (photo == "null" || photo == "") {
                             lengkapiProfil.visibility = View.VISIBLE
                             imgWarning.visibility = View.VISIBLE
                         } else {
-                            Picasso.get().load(photo).into(profilPic)
+                            Picasso.get().load(photo).into(root.profilPic)
                         }
                     }
                 }
@@ -100,4 +119,40 @@ class ProfileFragment : Fragment() {
         })
     }
 
+    private fun openImageChooser(view: View){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            imageUri = data.data!!
+            uploadImage()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun uploadImage() {
+        if (imageUri != null) {
+            var donloturi = ""
+            val filereference = storage.child(auth.currentUser?.uid.toString() + "." + getFileExtension(imageUri))
+            filereference.putFile(imageUri)
+            Handler().postDelayed({
+                filereference.downloadUrl.addOnSuccessListener {
+                    donloturi = it.toString()
+                    ref.child(auth.currentUser?.uid!!).child("photo").setValue(donloturi)
+                }
+            }, 5000)
+        } else {
+            Toast.makeText(root.context, "Tidak ada fail yang dipilih", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getFileExtension(uri: Uri): String {
+        val cr = root.context.contentResolver
+        val mime = MimeTypeMap.getSingleton()
+        return mime.getExtensionFromMimeType(cr.getType(uri))!!
+    }
 }
