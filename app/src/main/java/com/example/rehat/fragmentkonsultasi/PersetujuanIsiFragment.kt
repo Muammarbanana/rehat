dcda50b820839ce337c1e38a1ad7fe8d4feab3c6
Muammarbanana <muammarfanani2@gmail.com>
2020-06-26 10:48:28 +0700

    add batalkan janji feature

--- app/src/main/java/com/example/rehat/fragmentkonsultasi/PersetujuanIsiFragment.kt
+++ app/src/main/java/com/example/rehat/fragmentkonsultasi/PersetujuanIsiFragment.kt
@@ -1,18 +1,22 @@
 package com.example.rehat.fragmentkonsultasi
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.rehat.R
 import com.example.rehat.RincianPersetujuan
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_persetujuan_isi.*
 import kotlinx.android.synthetic.main.fragment_persetujuan_isi.view.*
+import kotlinx.android.synthetic.main.pop_alert.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -63,6 +67,11 @@ class PersetujuanIsiFragment : Fragment() {
                         root.agrTanggal.contentDescription = "Hari dan tanggal, $tanggal"
                         root.agrJam.contentDescription = "Pukul, $jam WIB"
                         root.agrCatatan.contentDescription = "Catatan tambahan, $catatan"
+                        if (teksBatalJanji != null) {
+                            teksBatalJanji.visibility = View.VISIBLE
+                            teksBatalJanji.setOnClickListener { popAlert(idjanji, root) }
+                        }
+                        shiftCardLayout(1)
                         when (status.toInt()) {
                             1 -> {
                                 agrBtnStatus.text = "Diterima"
@@ -73,6 +82,8 @@ class PersetujuanIsiFragment : Fragment() {
                                     intent.putExtra("idJanji", idjanji)
                                     root.context.startActivity(intent)
                                 }
+                                teksBatalJanji.visibility = View.GONE
+                                shiftCardLayout(2)
                             }
                             2 -> {
                                 agrBtnStatus.text = "Ditolak"
@@ -83,6 +94,8 @@ class PersetujuanIsiFragment : Fragment() {
                                     intent.putExtra("idJanji", idjanji)
                                     root.context.startActivity(intent)
                                 }
+                                teksBatalJanji.visibility = View.GONE
+                                shiftCardLayout(2)
                             }
                         }
                     }
@@ -133,6 +146,60 @@ class PersetujuanIsiFragment : Fragment() {
                 }
             }
         })
+    }
+
+    private fun popAlert(idjanji: String, view: View) {
+        val dialog = AlertDialog.Builder(view.context).create()
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.pop_alert, null)
+        dialog.setView(dialogView)
+        dialog.setCancelable(true)
+        dialogView.cardAlert.radius = 10F
+        dialogView.alertText.text = "Apakah kamu yakin ingin membatalkan janji pertemuan?"
+        dialogView.btnAccept.text = "Ya, Yakin"
+        dialogView.btnAccept.setTextColor(Color.parseColor("#DB4437"))
+        dialogView.btnCancel.setOnClickListener { dialog.dismiss() }
+        dialogView.btnAccept.setOnClickListener {
+            deleteJanji(idjanji)
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun deleteJanji(id: String) {
+        ref = FirebaseDatabase.getInstance().getReference("janji")
+        ref.child(id).removeValue()
+    }
+
+    private fun shiftCardLayout(jenis: Int) {
+        when(jenis) {
+            1 -> {
+                root.guideline188.setGuidelinePercent(0.8386F)
+                root.guideline189.setGuidelinePercent(0.1388F)
+                root.guideline193.setGuidelinePercent(0.1768F)
+                root.guideline194.setGuidelinePercent(0.3536F)
+                root.guideline195.setGuidelinePercent(0.3902F)
+                root.guideline196.setGuidelinePercent(0.4451F)
+                root.guideline197.setGuidelinePercent(0.4817F)
+                root.guideline198.setGuidelinePercent(0.5365F)
+                root.guideline199.setGuidelinePercent(0.5792F)
+                root.guideline200.setGuidelinePercent(0.8597F)
+                root.guideline201.setGuidelinePercent(0.8780F)
+            }
+            else -> {
+                root.guideline188.setGuidelinePercent(0.9322F)
+                root.guideline189.setGuidelinePercent(0.1230F)
+                root.guideline193.setGuidelinePercent(0.1526F)
+                root.guideline194.setGuidelinePercent(0.3052F)
+                root.guideline195.setGuidelinePercent(0.3368F)
+                root.guideline196.setGuidelinePercent(0.3842F)
+                root.guideline197.setGuidelinePercent(0.4157F)
+                root.guideline198.setGuidelinePercent(0.4631F)
+                root.guideline199.setGuidelinePercent(0.5F)
+                root.guideline200.setGuidelinePercent(0.7605F)
+                root.guideline201.setGuidelinePercent(0.8026F)
+            }
+        }
     }
 
 }
